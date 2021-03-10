5bf96be4f92fd34fdfc64a245fcda5e0fe6d8d58
unknown <aangaymoi@gmail.com>
2021-03-10 22:45:42 +0700

    first-commit.

---
+++ AzureServiceBusExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Threading.Tasks;
+
+namespace AzureServiceBusExample
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            await Test01();
+            Console.ReadLine();
+        }
+
+        private async static Task Test01()
+        {
+            string connectionString = "<This is your endpoint connectionString>";
+            string queueName = "<your queue name>";
+
+            // since ServiceBusClient implements IAsyncDisposable we create it with "await using"
+            await using var client = new ServiceBusClient(connectionString);
+
+            // create the sender
+            ServiceBusSender sender = client.CreateSender(queueName);
+
+            // create a message that we can send. UTF-8 encoding is used when providing a string.
+            ServiceBusMessage message = new ServiceBusMessage("Hello world!");
+
+            // send the message
+            await sender.SendMessageAsync(message);
+
+            // create a receiver that we can use to receive the message
+            ServiceBusReceiver receiver = client.CreateReceiver(queueName);
+
+            // the received message is a different type as it contains some service set properties
+            ServiceBusReceivedMessage receivedMessage = await receiver.ReceiveMessageAsync();
+
+            // get the message body as a string
+            string body = receivedMessage.Body.ToString();
+            Console.WriteLine(body);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# AzureServiceBusExample
+     
+	 
+	 How to create Service Bus
+     - Azure / Integration / Service Bus
+       Add
+       
+     - Go to your Service Bus choose
+       Shared access policies / RootManageSharedAccessKey / Manage / ConnectionString : Copy
+     
+     - Create your queue Name
+     
+     Add to project test
+     And see how it works  
